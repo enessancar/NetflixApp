3dfe4588c4dabb4e65b1e60c1c1fbe2642287672
enessancar <enes57751907@gmail.com>
2023-07-10 17:16:25 +0300

    Initial Commit

---
+++ NetflixApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixApp
+//
+//  Created by Enes Sancar on 10.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
